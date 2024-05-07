436fa1d8165acb023938a9cc022699204d507ad5
Nitin Yadav <97513795+tuffaniladka@users.noreply.github.com>
2024-05-07 21:41:10 +0530

    initial setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,25 @@
-import app from "./server.js";
-import connectDb from "./db/connection.js";
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { errorHandler, notFound } = require("./middleware/errorHandler");
+const userRoutes = require("./routes/User/userRoutes");
+//const photoRoutes = require("./routes/Photo/photoRoutes");
+//const commentRoutes = require("./routes/Comment/commentRoutes");
+require("./config/dbConnect")();
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//app.use(cookieParser());
+app.use(express.static(__dirname + "/public"));
+app.use("/api/users", userRoutes);
+//app.use("/api/photos", photoRoutes);
+//app.use("/api/comments", commentRoutes);
 
-connectDb()
-    .then(() => {
-        app.listen(process.env.PORT || 8000, () => {
-            console.log("App started listening on 8000")
-        })
-    })
+//error handler
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`server running on port ${process.env.PORT}`);
+});
---
+++ server/src/utils/validateMongodbID.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const validateMongodbID = (id) => {
+  const isValid = mongoose.Types.ObjectId.isValid(id);
+  if (!isValid) {
+    throw new Error("Id is not velid found");
+  }
+};
+
+module.exports = validateMongodbID;
