88dd1644522a8ab5075272c670c5d6c4c96460f4
Nitin Yadav <97513795+tuffaniladka@users.noreply.github.com>
2024-05-07 20:49:02 +0530

    hash password

--- server/src/models/report.model.js
+++ server/src/models/report.model.js
@@ -1,52 +1,50 @@
 import mongoose from "mongoose";
 
-const reportSchema = new mongoose.Schema({
+const reportSchema = new mongoose.Schema(
+  {
     createdBy: {
-        type: Schema.Types.ObjectId,
-        ref: "Worker"
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Worker",
     },
     area: {
-        type: String,
-        enum: ['Area 1', 'Area 2', 'Area 3', 'Area 4'],
-        required: true
+      type: String,
+      enum: ["Area 1", "Area 2", "Area 3", "Area 4"],
+      required: true,
     },
 
     topic: {
-        type: String,
-        enum: ['Before Work', 'After Work', 'Complain'],
-        required: true
+      type: String,
+      enum: ["Before Work", "After Work", "Complain"],
+      required: true,
     },
 
     chemical: {
-        type: String,
-        enum: ["Acid", "Alkaline", "Water"],
-
+      type: String,
+      enum: ["Acid", "Alkaline", "Water"],
     },
     premise: {
-        type: String,
+      type: String,
     },
     tempature: {
-        type: String,
+      type: String,
     },
     rating: {
-        type: String,
-        enum: [20, 30, 40, 50, 60, 70, 80, 90, 100]
+      type: String,
+      enum: [20, 30, 40, 50, 60, 70, 80, 90, 100],
     },
     test: {
-        type: String,
+      type: String,
     },
     problem: {
-        type: String,
-        enum: ["Water", "Hose", "Covering Up", "Another"]
+      type: String,
+      enum: ["Water", "Hose", "Covering Up", "Another"],
     },
     comment: {
-        type: String,
+      type: String,
     },
-    photo: {
-        type: String,
-    }
-
-}, { timestamps: true })
+    photo: { type: [String] },
+  },
+  { timestamps: true }
+);
 
 export const Report = mongoose.model("Report", reportSchema);
-
--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,24 +1,41 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     factoryName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     role: {
-        type: String,
-        enum: ["Admin", "User", "Worker"],
-        required: true
+      type: String,
+      enum: ["Admin", "User", "Worker"],
+      required: true,
     },
     password: {
-        type: String,
-        required: true
-    }
-}, { timestamps: true })
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-export const User = mongoose.model("User", userSchema)
+//Hash password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
