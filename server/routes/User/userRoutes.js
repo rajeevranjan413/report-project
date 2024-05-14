edb062afcc14979b2cc8b95b20f39a2c633261dd
Nitin Yadav <97513795+tuffaniladka@users.noreply.github.com>
2024-05-14 23:47:46 +0530

    get client list route

--- server/routes/User/userRoutes.js
+++ server/routes/User/userRoutes.js
@@ -6,18 +6,18 @@ const {
   getEmployeeList,
   getManagerList,
   deleteUserCtrl,
+  getClientList,
 } = require("../../controller/users/usersCtrl");
 const authMiddleware = require("../../middleware/authMiddleware");
 
 const userRoutes = express.Router();
-
 
 userRoutes.post("/createUser", createUserCtrl);
 userRoutes.post("/login", loginUserCtrl);
 userRoutes.get("/checkLogged", authMiddleware, checkLoggedCtrl);
 userRoutes.get("/employeeList", authMiddleware, getEmployeeList);
 userRoutes.get("/managerList", authMiddleware, getManagerList);
+userRoutes.get("/clientList", authMiddleware, getClientList);
 userRoutes.delete("/deleteUser/:id", authMiddleware, deleteUserCtrl);
-
 
 module.exports = userRoutes;
